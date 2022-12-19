597049e817c0ab72967b36263bc128e7d8e01ef9
anza-afk <zapokami@gmail.com>
2022-12-19 14:05:56 +0300

    connect to db

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from config import settings
+from db.database import Base, engine
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def create_tables():
+	Base.metadata.create_all(bind=engine)
+
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	configure_static(app)
+	create_tables()
+	return app
+
+app = start_application()
 
 @app .get("/", response_class=HTMLResponse)
 def home(request: Request):
